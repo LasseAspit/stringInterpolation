88d1bf25a17736e0d320c21237ae392f366b8368
3kant50075 <3kant50075@CV-BB-5858>
2021-05-26 11:33:56 +0200

    Add project files.

---
+++ stringInterpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace stringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var date = DateTime.Now;
+                string navn1 = "Kasper";
+                string navn2 = "Karsten";
+                string navn3 = "Kenneth";
+                navn3 = "Susanne";
+                Console.WriteLine($"Hello, {navn1}! today is {date.DayOfWeek}, it is {date:HH:mm} now.");
+            }
+
+
+        }
+    }
+}
